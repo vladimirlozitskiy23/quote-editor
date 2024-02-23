9989e2a8128c51b0ffde06246923a60b74b42bfb
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2024-02-23 13:20:11 +0300

    Add turbo frame and turbo stream examples

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,7 +3,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered
   end
 
   def show
@@ -17,7 +17,10 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end  
     else
       render :new
     end
@@ -36,7 +39,10 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream
+    end
   end
 
   private
